7dce32df77923b69d6016995b0df6a6832908c54
nmalikgeminie <nmalik@geminiesolutions.com>
2023-02-21 19:03:06 +0500

    some refactoring

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
-import {CommonModule, DatePipe} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
-import {MasterDetailComponent} from './components/master-detail/master-detail.component';
+import { CommonModule, DatePipe } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { MasterDetailComponent } from './components/master-detail/master-detail.component';
 import { OptionDropdownComponent } from './components/option-dropdown/option-dropdown.component';
-import {SafePipe} from './safe.pipe';
+import { SafePipe } from './safe.pipe';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -31,6 +31,7 @@ import { MatIconModule } from '@angular/material/icon';
   exports: [
     MasterDetailComponent,
     OptionDropdownComponent,
+    PageNotFoundComponent,
     SafePipe,
   ],
   providers: [DatePipe]
